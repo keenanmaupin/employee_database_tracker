095258430d2b44a0a1df1fffd550a31183aeeb42
keenan maupin <maupin.keenan97@gmail.com>
2024-10-05 14:44:16 -0700

    added SQL seeds/schema/query

--- SQL/query.sql
+++ SQL/query.sql
@@ -1,3 +1,9 @@
-SELECT [ADD HERE] AS [ADD HERE] AS [ADD HERE]
-FROM [ADD HERE]
-JOIN [ADD HERE] ON [ADD HERE]
+SELECT name, title, salary, first_name, last_name, manager_id 
+FROM department
+INNER JOIN role
+ON department.id = role.department_id
+INNER JOIN employee
+ON role.id = employee.role_id
+
+
+     
--- SQL/seeds.sql
+++ SQL/seeds.sql
@@ -1,13 +1,14 @@
-INSERT INTO [ADD HERE] ([ADD HERE] )
-VALUES [ADD HERE] 
-	   
-INSERT INTO [ADD HERE] ([ADD HERE] )
-VALUES [ADD HERE] 
-       
-INSERT INTO [ADD HERE] ([ADD HERE] )
-VALUES [ADD HERE] 
+INSERT INTO department(name)
+VALUES ('Kitchen'),
+       ('Cashier'),
+       ('Janitor');
 
-       
-INSERT INTO [ADD HERE] ([ADD HERE] )
-INSERT INTO [ADD HERE] ([ADD HERE] )
-VALUES [ADD HERE] 
+INSERT INTO role(title, salary, department_id)
+VALUES ('Chef', 5.00, 1),
+       ('main_cashier',10.00, 2),
+	   ('mast_custodian',50.00, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Silly','Goose',1,1),
+       ('Quick','Quack',2,0),
+       ('Mallard','Zucklberg',3,0);
